e8a87ac486cb8ad6daecf17f056475af1d6945a5
Bazán Romina <danielabazan94@gmail.com>
2020-10-22 08:24:29 -0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Mi clìnica',
+    'version': '1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Modulo para sanatorios/clinicas/hospitales.',
+    'author': 'Romina Bazan',
+    'maintainer': 'Romina Bazan',
+    'depends': [
+                #'base',
+                #'contacts', 
+                #'website'
+                ],
+    'data': [
+        'views/obrasociales_view.xml',
+        'views/pacientes_view.xml',
+        #'views/menu_view.xml',
+        #'data/mail_template.xml',
+    ],
+    #'images': ['static/description/banner.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
